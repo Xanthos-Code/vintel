1dd8edbaa7e49257cc15818f9e108a78c689c9b2
Mark Kieling <xanthos-eve>
2016-07-17 17:31:59 -0700

    Exclude scrollbars from panning calculations and event handling

--- src/vi/PanningWebView.py
+++ src/vi/PanningWebView.py
@@ -1,5 +1,5 @@
 
-from PyQt5.QtCore import Qt, QPoint, QEvent, pyqtSignal, QUrl
+from PyQt5.QtCore import Qt, QPoint, QRect, QEvent, pyqtSignal, QUrl
 from PyQt5.QtGui import QMouseEvent
 from PyQt5.QtWidgets import QApplication
 from vi.ui.viui import MainWindow
@@ -24,94 +24,114 @@ class PanningWebView(QWebView if MainWindow.oldStyleWebKit else QWebEngineView):
         self.position = None
         self.offset = QPoint(0, 0)
         self.handIsClosed = False
-        if not MainWindow.oldStyleWebKit:
-            self.setPage(VintelSvgPage(parent=self))
+        self.clickedInScrollBar = False
+        self.widget = None
+        self.setPage(VintelSvgPage(parent=self))
+
+
+    def setPage(self, newPage):
+        super(PanningWebView, self).setPage(newPage)
+        if MainWindow.oldStyleWebKit:
+            self.widget = self.page().mainFrame()
+        else:
+            self.widget = self.page()
 
 
     def mousePressEvent(self, mouseEvent):
-        if self.ignored.count(mouseEvent):
-            self.ignored.remove(mouseEvent)
-            return super(PanningWebView, self).mousePressEvent(mouseEvent)
+        pos = mouseEvent.pos()
 
-        if not self.pressed and not self.scrolling and mouseEvent.modifiers() == Qt.NoModifier:
-            if mouseEvent.buttons() == Qt.LeftButton:
-                self.pressed = True
-                self.scrolling = False
-                self.handIsClosed = False
-                QApplication.setOverrideCursor(Qt.OpenHandCursor)
+        if mouseEvent.buttons() == Qt.LeftButton and (self.pointInScroller(pos, Qt.Vertical) or self.pointInScroller(pos, Qt.Horizontal)):
+            self.clickedInScrollBar = True
+        else:
+            if self.ignored.count(mouseEvent):
+                self.ignored.remove(mouseEvent)
+                return super(PanningWebView, self).mousePressEvent(mouseEvent)
 
-                self.position = mouseEvent.pos()
-                if MainWindow.oldStyleWebKit:
-                    frame = self.page().mainFrame()
-                else:
-                    frame = self.page()
-                scrollx = frame.evaluateJavaScript("window.scrollX")
-                scrolly = frame.evaluateJavaScript("window.scrollY")
-                self.offset = QPoint(scrollx, scrolly)
-                return
+            if not self.pressed and not self.scrolling and mouseEvent.modifiers() == Qt.NoModifier:
+                if mouseEvent.buttons() == Qt.LeftButton:
+                    self.pressed = True
+                    self.scrolling = False
+                    self.handIsClosed = False
+                    QApplication.setOverrideCursor(Qt.OpenHandCursor)
+
+                    self.position = mouseEvent.pos()
+                    scrollx = self.widget.evaluateJavaScript("window.scrollX")
+                    scrolly = self.widget.evaluateJavaScript("window.scrollY")
+                    self.offset = QPoint(scrollx, scrolly)
+                    return
 
         return super(PanningWebView, self).mousePressEvent(mouseEvent)
 
 
     def mouseReleaseEvent(self, mouseEvent):
-        if self.ignored.count(mouseEvent):
-            self.ignored.remove(mouseEvent)
-            return super(PanningWebView, self).mousePressEvent(mouseEvent)
+        if self.clickedInScrollBar:
+            self.clickedInScrollBar = False
+        else:
+            if self.ignored.count(mouseEvent):
+                self.ignored.remove(mouseEvent)
+                return super(PanningWebView, self).mousePressEvent(mouseEvent)
 
-        if self.scrolling:
-            self.pressed = False
-            self.scrolling = False
-            self.handIsClosed = False
-            QApplication.restoreOverrideCursor()
-            return
+            if self.scrolling:
+                self.pressed = False
+                self.scrolling = False
+                self.handIsClosed = False
+                QApplication.restoreOverrideCursor()
+                return
 
-        if self.pressed:
-            self.pressed = False
-            self.scrolling = False
-            self.handIsClosed = False
-            QApplication.restoreOverrideCursor()
+            if self.pressed:
+                self.pressed = False
+                self.scrolling = False
+                self.handIsClosed = False
+                QApplication.restoreOverrideCursor()
 
-            event1 = QMouseEvent(QEvent.MouseButtonPress, self.position, Qt.LeftButton, Qt.LeftButton, Qt.NoModifier)
-            event2 = QMouseEvent(mouseEvent)
+                event1 = QMouseEvent(QEvent.MouseButtonPress, self.position, Qt.LeftButton, Qt.LeftButton, Qt.NoModifier)
+                event2 = QMouseEvent(mouseEvent)
 
-            self.ignored.append(event1)
-            self.ignored.append(event2)
+                self.ignored.append(event1)
+                self.ignored.append(event2)
 
-            QApplication.postEvent(self, event1)
-            QApplication.postEvent(self, event2)
-            return
+                QApplication.postEvent(self, event1)
+                QApplication.postEvent(self, event2)
+                return
 
         return super(PanningWebView, self).mouseReleaseEvent(mouseEvent)
 
 
     def mouseMoveEvent(self, mouseEvent):
-        if self.scrolling:
-            if not self.handIsClosed:
-                QApplication.restoreOverrideCursor()
-                QApplication.setOverrideCursor(Qt.ClosedHandCursor)
-                self.handIsClosed = True
-            delta = mouseEvent.pos() - self.position
-            p = self.offset - delta
+        if not self.clickedInScrollBar:
+            if self.scrolling:
+                if not self.handIsClosed:
+                    QApplication.restoreOverrideCursor()
+                    QApplication.setOverrideCursor(Qt.ClosedHandCursor)
+                    self.handIsClosed = True
+                delta = mouseEvent.pos() - self.position
+                p = self.offset - delta
+                self.widget.evaluateJavaScript('window.scrollTo({}, {});'.format(p.x(), p.y()))
+                return
 
-            if MainWindow.oldStyleWebKit:
-                frame = self.page().mainFrame()
-            else:
-                frame = self.page()
-            frame.evaluateJavaScript('window.scrollTo({}, {});'.format(p.x(), p.y()))
-            return
-
-        if self.pressed:
-            self.pressed = False
-            self.scrolling = True
-            return
+            if self.pressed:
+                self.pressed = False
+                self.scrolling = True
+                return
 
         return super(PanningWebView, self).mouseMoveEvent(mouseEvent)
+
+
+    def pointInScroller(self, position, orientation):
+        rect = self.widget.scrollBarGeometry(orientation)
+        topLeft = self.mapToGlobal(rect.topLeft())
+        bottomRight = self.mapToGlobal(rect.bottomRight())
+        globalRect = QRect(topLeft, bottomRight)
+        return globalRect.contains(self.mapToGlobal(position))
 
 
 class VintelSvgPage(QWebPage if MainWindow.oldStyleWebKit else QWebEnginePage):
 
     def __init__(self, parent=None):
-        QWebEnginePage.__init__(self, parent)
+        if MainWindow.oldStyleWebKit:
+            QWebPage.__init__(self, parent)
+        else:
+            QWebEnginePage.__init__(self, parent)
 
 
     def acceptNavigationRequest(self, url, type, isMainFrame):
