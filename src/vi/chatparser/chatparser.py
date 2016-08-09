f0fd050bdc1ef737d39a446734f8562dbe4cb63e
Mark Kieling <xanthos-eve>
2016-08-08 19:06:51 -0700

    No ascii in source - no good.

--- src/vi/chatparser/chatparser.py
+++ src/vi/chatparser/chatparser.py
@@ -33,7 +33,7 @@ from .parser_functions import parseStatus
 from .parser_functions import parseUrls, parseShips, parseSystems
 
 # Names the local chatlogs could start with (depends on l10n of the client)
-LOCAL_NAMES = ("Локальный", "Lokal", "Local")
+LOCAL_NAMES = ("Local", "Lokal", six.text_type("\u041B\u043E\u043A\u0430\u043B\u044C\u043D\u044B\u0439"))
 
 
 class ChatParser(object):
